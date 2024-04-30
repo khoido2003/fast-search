b7bc1e8551327583d124aa95c138934ddc90b3f9
Khoi Do <khoizpro31@gmail.com>
2024-04-30 21:47:48 +0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="relative min-h-screen isolate"></div>
+
+        {children}
+      </body>
     </html>
   );
 }
